5bc076671ffc5161cd85f07b2bab5b79498a8634
AdriaCara <adriaProgrammer@gmail.com>
2022-04-06 18:21:49 +0200

    New changes

--- body.php
+++ body.php
@@ -2,27 +2,25 @@
     <div>
         <h1><?php echo $nomDePagina ?></h1>
 
-        <p> 
-            <?php 
-                if (count($_COOKIE) > 0) {
-                    echo "<p>Hola ".$_COOKIE[$cookie_name]."</p>";
-                } else {
-                    echo " anonim";
-                }
-            ?>
-        </p>
-        <p><?php 
-                if (isset($_SESSION['email'])) {
-                    echo "<p>El teu correu és: [ ".$_SESSION['email']." ]</p>";
-                } 
-            ?>
-        <p><?php 
-                if (isset($_SESSION['contrasena'])) {
-                    echo "<p>La teva contrasenya és: [ ".$_SESSION['contrasena']." ]</p>";
-                }
-            ?>
-            <?php
-                echo "<p>Numero d'accions: [ ".$_SESSION['contador']." ]</p>";
-            ?>
+        <?php 
+            if (count($_COOKIE) > 0) {
+                echo "<p>Hola ".$_COOKIE[$cookie_name]."</p>";
+            } else {
+                echo " anonim";
+            }
+        ?>
+        <?php 
+            if (isset($_SESSION['email'])) {
+                echo "<p>El teu correu és: [ ".$_SESSION['email']." ]</p>";
+            } 
+        ?>
+        <?php 
+            if (isset($_SESSION['contrasena'])) {
+                echo "<p>La teva contrasenya és: [ ".$_SESSION['contrasena']." ]</p>";
+            }
+        ?>
+        <?php
+            echo "<p>Numero d'accions: [ ".$_SESSION['contador']." ]</p>";
+        ?>
     </div>
 </div>
