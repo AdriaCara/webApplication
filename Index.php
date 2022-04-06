1bd2675b481829b3acddd51f57a9e5ddc66eca87
AdriaCara <adriaProgrammer@gmail.com>
2022-04-06 19:34:13 +0200

    New little changes

--- Index.php
+++ Index.php
@@ -1,35 +1,24 @@
 <!-- Header, navbar and footer -->
 <?php
 
-    if(isset($_POST['nomUsuari'])) {
-            $nomUsuari = $_POST['nomUsuari'];
-    }
-    if (isset($_POST['email'])) {
-        $email = $_POST['email'];
-    }
-
-    if (isset($_POST['contrasena'])) {
-        $contrasena = $_POST['contrasena'];
-    }
-
     session_start();
 
-    if (isset($nomUsuari)) {
-        $_SESSION['nomUsuari'] = $nomUsuari;
+    if (isset($_POST['nomUsuari'])) {
+        $_SESSION['nomUsuari'] = $_POST['nomUsuari'];
     }
-    if (isset($email)) {
-        $_SESSION['email'] = $email;
+    if (isset($_POST['email'])) {
+        $_SESSION['email'] = $_POST['email'];
     }
-    if (isset($contrasena)) {
-        $_SESSION['contrasena'] = $contrasena;
+    if (isset($_POST['contrasena'])) {
+        $_SESSION['contrasena'] = $_POST['contrasena'];
     }
     if( isset( $_SESSION['contador'] ) ) {
         $_SESSION['contador'] += 1;
     }else {
         $_SESSION['contador'] = 1;
-        }
+    }
 
-    if(isset($nomUsuari)) {
+    if(!isset($_COOKIE['usuario'])) {
         setcookie("usuario", $_POST['nomUsuari'], time() + (86400 * 30), "/");
     }
 
